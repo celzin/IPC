5c88d75e75f7e931e47639002e9fd2e756ec8484
Celso-WSL <celsovinicius4@gmail.com>
2025-01-19 16:53:38 -0300

    feat[validação]: criando validação de Nome e CPF

--- src/cliente.c
+++ src/cliente.c
@@ -1,12 +1,38 @@
 #include "includes/cliente.h"
 
-Cliente *criar_cliente(const char *nome, int cpf, int prioridade, int itens)
+int validar_nome(const char *nome)
+{
+    for (int i = 0; nome[i] != '\0'; i++)
+    {
+        if (!isalpha(nome[i]) && !isspace(nome[i]))
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int validar_cpf(const char *cpf)
+{
+    if (strlen(cpf) != 11)
+        return 0;
+
+    for (int i = 0; i < 11; i++)
+    {
+        if (!isdigit(cpf[i]))
+            return 0;
+    }
+
+    return 1; // CPF válido
+}
+
+Cliente *criar_cliente(const char *nome, const char *cpf, int prioridade, int itens)
 {
     Cliente *novo_cliente = (Cliente *)malloc(sizeof(Cliente));
     if (novo_cliente)
     {
         strncpy(novo_cliente->nome, nome, MAX_NOME);
-        novo_cliente->cpf = cpf;
+        strncpy(novo_cliente->cpf, cpf, MAX_CPF);
         novo_cliente->prioridade = prioridade;
         novo_cliente->itens = itens;
     }
--- src/includes/cliente.h
+++ src/includes/cliente.h
@@ -1,18 +1,23 @@
 #ifndef CLIENTE_H
 #define CLIENTE_H
 
-#define MAX_NOME 100
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+#define MAX_NOME 100
+#define MAX_CPF 12 // 11 dígitos + '\0'
 
 typedef struct
 {
     char nome[MAX_NOME];
-    int cpf;
+    char cpf[MAX_CPF];
     int prioridade;
     int itens;
 } Cliente;
 
-Cliente *criar_cliente(const char *nome, int cpf, int prioridade, int itens);
+Cliente *criar_cliente(const char *nome, const char *cpf, int prioridade, int itens);
+int validar_nome(const char *nome);
+int validar_cpf(const char *cpf);
 
 #endif
